5e1e96e6a1fefc52490144ecfc39f2ebd8e89808
Joel Lim <joellimaojun@hotmail.com>
2021-03-17 12:48:12 +0800

    Add tables for experiment and update .gitignore

---
+++ experiment/setup.sh
@@ -0,0 +1,11 @@
+java RandomDB AIRCRAFTS 15000
+java RandomDB EMPLOYEES 15000
+java RandomDB FLIGHTS 15000
+java RandomDB SCHEDULE 15000
+java RandomDB CERTIFIED 15000
+
+java ConvertTxtToTbl AIRCRAFTS
+java ConvertTxtToTbl EMPLOYEES
+java ConvertTxtToTbl FLIGHTS
+java ConvertTxtToTbl SCHEDULE
+java ConvertTxtToTbl CERTIFIED
