d38ede75398afc7daf784f0bbb835d7e2ce9945b
Joel Lim <joellimaojun@hotmail.com>
2021-03-23 20:59:33 +0800

    Add demo setup files

---
+++ demo/setup.bat
@@ -0,0 +1,9 @@
+java RandomDB BILL 100
+java RandomDB CART 100
+java RandomDB CARTDETAILS 100
+java RandomDB CUSTOMER 100
+
+java ConvertTxtToTbl BILL
+java ConvertTxtToTbl CART
+java ConvertTxtToTbl CARTDETAILS
+java ConvertTxtToTbl CUSTOMER
---
+++ demo/test.bat
@@ -0,0 +1,4 @@
+java QueryMain join.sql join.out 8000 800 1
+java QueryMain distinct.sql distinct.out 8000 800 1
+java QueryMain orderby.sql orderby.out 8000 800 1
+java QueryMain groupby.sql groupby.out 8000 800 1
